777a5ea3480363de30ff8be9f70e86f2465f7348
Fabioiscoding <gurjinder07italy@gmail.com>
2025-03-29 08:12:46 +0100

    Aggiunta di size() e isEmpty()

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList l = new MyLinkedList();
+
+        l.addfirst(1);
+        l.addfirst(7);
+        l.addfirst(3);
+        l.add(2, 4);
+        System.out.println(l);
+        l.remove(2);
+        System.out.println(l);
+        System.out.println(l.size());
+        System.out.println(l.isEmpty());
+
+    }
+}
---
+++ src/MyLinkedList.java
@@ -0,0 +1,76 @@
+public class MyLinkedList{
+    private Nodo head;
+    private int n_items;
+
+    public MyLinkedList(){
+        this.head = null;
+        this.n_items = 0;
+    }
+
+    public void addfirst(int n){
+        Nodo temp = this.head;
+        this.head = new Nodo(n,temp);
+        this.n_items++;
+    }
+
+    public void add(int pos, int n) {
+        if (pos > n_items || pos < 0) {
+            throw new IllegalArgumentException("Posizione non valida!!");
+        }
+
+        if (pos == 0) {
+            head = new Nodo(n, head);
+        } else {
+            Nodo temp = head;
+            for (int i = 0; i < pos - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = new Nodo(n, temp.next);
+        }
+        n_items++;
+    }
+
+    public void removeFirst(){
+        if (this.n_items == 0)
+            throw new IllegalArgumentException("La dimensione della lista è 0!!");
+        this.head = this.head.next;
+        this.n_items--;
+    }
+
+    public void remove(int pos){
+        if (pos > n_items || pos < 0) {
+            throw new IllegalArgumentException("Posizione non valida!!");
+        }
+
+        if (pos == 0) {
+            this.removeFirst();
+        } else {
+            Nodo temp = head;
+            for (int i = 0; i < pos - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+
+        n_items--;
+    }
+
+    public int size(){
+        return n_items;
+    }
+
+    public boolean isEmpty(){
+        return n_items == 0;
+    }
+
+    @Override
+    public String toString(){
+        String s = "";
+        Nodo temp = head;
+        while(temp != null){
+            s += temp.info + " ";
+            temp = temp.next;
+        }
+        return s;
+    }
+}
